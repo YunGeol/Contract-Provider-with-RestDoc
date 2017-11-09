75c7b2abdf87046739935acdb9c03be719452435
이윤걸(YunGeol Lee)/개발혁신팀/SKP <lygirl@sk.com>
2017-11-09 14:21:35 +0900

    initail commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Contract-Provider-with-RestDoc
---
+++ src/main/java/com/example/providerrestdoc/ProviderRestdocApplication.java
@@ -0,0 +1,12 @@
+package com.example.providerrestdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderRestdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderRestdocApplication.class, args);
+	}
+}
